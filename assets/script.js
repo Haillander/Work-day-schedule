ac1763fc1a4eefdfd909c647a9fd8e0a8a0c6f27
Haillander <haillanderviana@gmail.com>
2022-12-12 16:14:43 +1100

    Variable schedules

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
+
 var today=moment().format("dddd, MMMM Do");
-$("#currentDay").text(today); 
+$("#currentDay").text(today);
+
 var hour=moment().format("H");
 console.log(hour);
 
 $( ".time-block" ).each(function() {
     console.log(this.dataset.hour);
+    
     if(this.dataset.hour===hour){
         $( this ).addClass( "present" );
     }
@@ -17,3 +20,14 @@ $( ".time-block" ).each(function() {
     }
     
   });
+
+  var schedules=[
+    {id:"9AM", text:""},
+    {id:"10AM", text:""},
+    {id:"11AM", text:""},
+    {id:"12PM", text:""},
+    {id:"1PM", text:""},
+    {id:"2PM", text:""},
+    {id:"3PM", text:""},
+    {id:"4PM", text:""},
+    {id:"5PM", text:""}];
