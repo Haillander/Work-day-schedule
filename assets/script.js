2dbf46001214580264c16ae1b99ccf08f63c1a36
Haillander <haillanderviana@gmail.com>
2022-12-14 16:13:36 +1100

    Code Comments Added

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-
+//Date displayed on top of calender 
 var today=moment().format("dddd, MMMM Do");
 $("#currentDay").text(today);
 
 var hour=moment().format("H");
 console.log(hour);
 
+//Timeclock indicates past, present and future 
 $( ".time-block" ).each(function() {
     console.log(this.dataset.hour);
     
@@ -21,6 +22,7 @@ $( ".time-block" ).each(function() {
     
   });
 
+// Schedule table 
   var schedules=[
     {id:"9AM", text:""},
     {id:"10AM", text:""},
@@ -31,3 +33,47 @@ $( ".time-block" ).each(function() {
     {id:"3PM", text:""},
     {id:"4PM", text:""},
     {id:"5PM", text:""}];
+
+    // Save bottom 
+    $(".saveBtn").on("click", function(e){
+        var text=$(e.target).prev().val();
+    
+    
+        var id=$(e.target).prev().attr("id");
+        console.log(id);
+    
+        let obj = schedules.find(o => o.id === id);
+        obj.text=$(e.target).prev().val();
+        
+        saveLocalStorage(schedules);
+        renderLocalStorage();
+    });
+    
+    function saveLocalStorage(savedSchedules){
+        localStorage.setItem("hourlySchedule",JSON.stringify(savedSchedules));
+        console.log("save");
+    };
+    
+    function renderLocalStorage()
+    {
+        var i=0;
+        $(".time-block").each(function(){        
+            console.log(i);
+            var todo=schedules[i].text;
+            console.log(todo);
+            $(this).text(schedules[i].text);
+            i++;
+        })
+    
+    }
+    function init(){
+        var storedSchedules = JSON.parse(localStorage.getItem("hourlySchedule"));
+    
+        if (storedSchedules !== null) {
+            schedules = storedSchedules;
+        }
+        console.log(schedules);
+        renderLocalStorage();
+    
+    }
+    init();
